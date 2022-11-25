dc72a5e05dababba6f2fc36e7c5925f922e4ac2c
pvwnthem <pvwn.them@gmail.com>
2022-11-25 12:20:49 -0500

    add boilerplate navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Navigation from './components/navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <p className='' >hello</p>
     </div>
   );
---
+++ src/components/navigation.tsx
@@ -0,0 +1,17 @@
+import React, { Component, useEffect } from 'react'
+
+type Props = {}
+
+type State = {}
+
+class Navigation extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>navigation</div>
+    )
+  }
+}
+
+export default Navigation
