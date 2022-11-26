48da9e9a89c3fdc25cf809c8373c8edaef16f97a
pvwnthem <pvwn.them@gmail.com>
2022-11-26 01:11:28 -0500

    e

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component, useEffect, useState } from 'react'
 import Navigation from './components/navigation'
 
 import "./App.css";
-import { ethers } from "ethers";
 
 function App() {
   
